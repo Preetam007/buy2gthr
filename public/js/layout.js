9cf252f82be8ab5bc98ce39d1a7e1f16bf1f79a1
Preetam007 <raopreetam007@gmail.com>
2015-03-29 22:44:47 +0530

    sunday dust changes
    sunday dust changes

--- public/js/layout.js
+++ public/js/layout.js
@@ -3,29 +3,29 @@ $(function () { $("[data-toggle='tooltip']").tooltip(); });
 
 
 
-
-
-
-
-
-
+$("#logout").click(function()
+{
+	localStorage.clear();
+});
 var previousScroll = 0,
-    headerOrgOffset = $('#navbar1').height();
+headerOrgOffset = $('#navbar1').height();
     //console.log(headerOrgOffset);
 $('#header-wrap').height($('#navbar1').height());
 
 $(window).scroll(function () {
     var currentScroll = $(this).scrollTop();
 	//console.log(currentScroll);
-    if (currentScroll > headerOrgOffset) {
-        if (currentScroll > 60) {
+    if (currentScroll > headerOrgOffset) 
+	{
+        if (currentScroll > 60) 
+		{
             $('#navbar2').slideUp();
 			$('#header-wrap').addClass("navbar-fixed-top");
         } 
     } 
 	if(currentScroll == 0)
 	{
-	  $('#header-wrap').removeClass("navbar-fixed-top");
+	   $('#header-wrap').removeClass("navbar-fixed-top");
 	   $('#navbar2').slideDown();
 	}
     previousScroll = currentScroll;
